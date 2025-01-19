428606819aa406f2f944cc278ab695ab21ca56b5
adminnubax <nahuel.riquelme@nubax.com.ar>
2025-01-19 19:57:04 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,1 @@
-pfbackup -U https://10.0.8.1:4433 -u admin -p CATAmarca6 -f /home/user1/ansible/backup/backup.fw.xml --no-verify
+/snap/bin/pfbackup -U https://10.0.8.1:4433 -u admin -p CATAmarca6 -f /home/user1/ansible/backup/backup.fw.xml --no-verify
